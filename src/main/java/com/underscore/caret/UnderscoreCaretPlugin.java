ff7cc78329b26ab03748952cf9bc721d2a393a9a
swarupsesha <swarupsesha@gmail.com>
2022-10-09 17:28:10 -0500

    Add files via upload

--- src/main/java/com/underscore/caret/UnderscoreCaretPlugin.java
+++ src/main/java/com/underscore/caret/UnderscoreCaretPlugin.java
@@ -8,22 +8,20 @@ import com.intellij.openapi.editor.event.EditorFactoryEvent;
 import com.intellij.openapi.editor.event.EditorFactoryListener;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
 import java.util.Arrays;
 
 import static com.intellij.openapi.editor.CaretVisualAttributes.Shape.UNDERSCORE;
 import static com.intellij.openapi.editor.CaretVisualAttributes.Weight.THIN;
-import static java.awt.Color.WHITE;
 
 public class UnderscoreCaretPlugin extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-
         EditorFactoryListener editorFactoryListener = new EditorFactoryListener() {
             @Override
             public void editorCreated(@NotNull EditorFactoryEvent editorFactoryEvent) {
-                CaretVisualAttributes c = new CaretVisualAttributes(WHITE, THIN, UNDERSCORE, 0.01f);
                 Arrays.stream(EditorFactory.getInstance().getAllEditors()).forEach(editor -> {
-                    editor.getCaretModel().getCurrentCaret().setVisualAttributes(c);
+                    editor.getCaretModel().getCurrentCaret().setVisualAttributes(new CaretVisualAttributes(new Color(220, 250, 138), THIN, UNDERSCORE, 0.1f));
                 });
             }
         };
